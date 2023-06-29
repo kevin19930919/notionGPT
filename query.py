229dfc2d9b58b407ebdfd45543583bfcf1d1efbd
kevin_tsai <kevin19930919@gmail.com>
2023-06-29 14:44:06 +0800

    feat: refactor chain class

--- query.py
+++ query.py
@@ -8,19 +8,20 @@ Given the following conversation and a follow up question, rephrase the follow u
 Follow Up Input: {question}
 Standalone question:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
-print(CONDENSE_QUESTION_PROMPT)
 
-def get_chain(vectorstore):
-    llm = OpenAI(
-        temperature=0, 
-        openai_api_key="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",
-        model_name='gpt-3.5-turbo'
-    )
+class QAChain() :
+    def __init__(self, vectorstore, llm) :
+        self.vectorstore = vectorstore
+        self.llm = llm
+        self.chain = self.new_chain(self.vectorstore, self.llm)
     
-    print(llm.model_name)
-    qa_chain = ConversationalRetrievalChain.from_llm(
-        llm,
-        vectorstore.as_retriever(),
-        condense_question_prompt=CONDENSE_QUESTION_PROMPT,
-    )
-    return qa_chain
+    def new_chain(self, vectorstore, llm):
+        return ConversationalRetrievalChain.from_llm(
+            llm,
+            vectorstore.as_retriever(),
+            condense_question_prompt=CONDENSE_QUESTION_PROMPT,
+        )
+    
+    def query(self, question) :
+        result = self.chain({"question": question, "chat_history": []})
+        return result
