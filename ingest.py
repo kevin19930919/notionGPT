fdddefac960094e21333674dbcb5c35d6261f395
kevin_tsai <kevin19930919@gmail.com>
2023-06-29 15:38:33 +0800

    feat: hide sensitive info

--- ingest.py
+++ ingest.py
@@ -1,9 +1,18 @@
+"""
+This script is used to embedding your notion data, and store as file.
+"""
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import NotionDirectoryLoader
 from langchain.vectorstores.faiss import FAISS
 
 from langchain.embeddings import OpenAIEmbeddings
 import pickle
+import configparser
+
+secrets = configparser.ConfigParser()
+secrets.read('secret.ini')
+
+OPENAI_API_KEY = secrets['DEFAULT']['OPENAI_API_KEY']
 
 # Load Data
 loader = NotionDirectoryLoader("NotionDB")
@@ -14,7 +23,7 @@ text_splitter = RecursiveCharacterTextSplitter()
 documents = text_splitter.split_documents(raw_documents)
 
 
-embeddings = OpenAIEmbeddings(openai_api_key="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")
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 # # Load Data to vectorstore
 vectorstore = FAISS.from_documents(documents, embeddings)
 
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pickle
 from query import QAChain
 from langchain.llms import OpenAI
+import configparser
+
+secrets = configparser.ConfigParser()
+secrets.read('secret.ini')
+
+OPENAI_API_KEY = secrets['DEFAULT']['OPENAI_API_KEY']
 
 
 if __name__ == "__main__":
@@ -11,7 +17,7 @@ if __name__ == "__main__":
     # initial llm
     llm = OpenAI(
         temperature=0, 
-        openai_api_key="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",
+        openai_api_key=OPENAI_API_KEY,
         model_name='gpt-3.5-turbo'
     )
     
