7e0480f94cf73fdc690d6e0493cc458eaa23a05f
kevin_tsai <kevin19930919@gmail.com>
2023-07-09 02:48:47 +0800

    feat: add get vactor data for notion db

--- adapter/notion/notion_server.py
+++ adapter/notion/notion_server.py
@@ -4,6 +4,10 @@ and loading documents from the langchain.document_loaders module.
 """
 import configparser
 from langchain.document_loaders import NotionDBLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+
+from langchain.embeddings import OpenAIEmbeddings
 
 # since we load secret in multiple place, this may not a good practice
 # tend to extract the secret loading to a single place
@@ -12,6 +16,7 @@ secrets.read('../../secret.ini')
 
 NOTION_API_KEY = secrets['DEFAULT']['NOTION_API_KEY']
 NOTION_ROOT_DB_ID = secrets['DEFAULT']['NOTION_ROOT_DB_ID']
+OPENAI_API_KEY = secrets['DEFAULT']['OPENAI_API_KEY']
 
 
 class NotionDB:
@@ -22,16 +27,32 @@ class NotionDB:
     def __init__(self, notion_api_token: str, db_id: list[tuple]):
         self.notion_api_token = notion_api_token
         self.db_id = db_id
-        self.loader = NotionDBLoader(
-            integration_token=NOTION_API_KEY,
-            database_id=NOTION_ROOT_DB_ID,
-            request_timeout_sec=30,  # optional
-        )   
-    def get_docs(self):
+        self.raw_data = self._get_raw_data()
+    def _get_raw_data(self):
         """
         Get the documents of the notion database.
 
         Returns:
             list[tuple]: The documents of the notion database.
         """
-        return self.loader.load()
+        return NotionDBLoader(
+            integration_token=NOTION_API_KEY,
+            database_id=NOTION_ROOT_DB_ID,
+            request_timeout_sec=30,  # optional
+        ).load()    
+    def get_vector_data(self):
+        """
+        Get the vectorstore of the notion database.
+        
+        Returns:
+            Matrix[Vector]: The Matrix of vectorstore of the notion database.
+        """
+        # Split text
+        text_splitter = RecursiveCharacterTextSplitter()
+        documents = text_splitter.split_documents(self.raw_data)
+
+
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+        # # Load Data to vectorstore
+        vectorstore = FAISS.from_documents(documents, embeddings)
+        return vectorstore
